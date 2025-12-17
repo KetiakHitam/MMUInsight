98570708993e18287119c319caf9bc07a161c151
Isac Megat Azlan <emegmeg2007@gmail.com>
2025-12-17 20:22:43 +0800

    translation framework is there and only flash messages are wrapped but im dropping this feature because it is stupidly tedious and not worth the time

--- app.py
+++ app.py
@@ -24,18 +24,15 @@ app.config['LANGUAGES'] = {
     'zh': 'Chinese'
 }
 
-db.init_app(app)
-bcrypt.init_app(app)
-login_manager.init_app(app)
-
-babel = Babel(app)
-
 def get_locale():
     if 'language' in session:
         return session['language']
     return request.accept_languages.best_match(app.config['LANGUAGES'].keys()) or 'en'
 
-app.jinja_env.globals['get_locale'] = get_locale
+db.init_app(app)
+bcrypt.init_app(app)
+login_manager.init_app(app)
+babel = Babel(app, locale_selector=get_locale)
 
 @login_manager.user_loader
 def load_user(user_id):
