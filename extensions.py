e30e9a46c7a1129345d2bc97da4a1d6cc380659e
softcycled <hominghao1710@gmail.com>
2026-01-03 11:50:35 +0800

    feat: added CSRF protection and email verification

--- extensions.py
+++ extensions.py
@@ -3,6 +3,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_wtf.csrf import CSRFProtect
+from flask_mail import Mail
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -13,4 +15,6 @@ limiter = Limiter(
     default_limits=["200 per day", "50 per hour"],
     storage_uri="memory://"
 )
+csrf = CSRFProtect()
+mail = Mail()
 
